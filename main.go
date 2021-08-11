6731bbe2cd657ca89bdaa71de6b8f4c2ae0896ac
root <root@raspberrypi>
2021-08-12 00:53:33 +0300

    Датчик max44009 барахлит добавил проверку значений и перезапрос

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "fmt"
+    "strconv"
     "net/http"
     "io/ioutil"
     "database/sql"
@@ -10,15 +11,24 @@ import (
 )
 
 const (
-     DB_USER     = "postgres"
+     DB_USER     = ""
      DB_PASSWORD = ""
      DB_NAME     = "ab_log_db"
 )
 
 
 func main() {
-        light := getLight("http://192.168.71.74/sec/?pt=31&scl=30&i2c_dev=max44009")
-        fmt.Println(light)
+        //counter чтобы получить точно значение с датчика
+        cntChecker := 0
+        var light string
+        for i := 1; i < 10; i++ {
+           light = getLight("http://192.168.71.74/sec/?pt=31&scl=30&i2c_dev=max44009")
+           if i == 10 || light !="NA"{ 
+                break
+           }
+        cntChecker += i
+        }
+        //fmt.Println(light)
         inserter(light)
 }
 
@@ -28,13 +38,28 @@ func inserter(light_val string) {
         db, err := sql.Open("postgres", dbinfo)
         checkErr(err)
         defer db.Close()
-
+        flight_val, err := strconv.ParseFloat(light_val, 64)
+        //иногда датчик выдает занчение больше 35000.0 или N/A
+        if err != nil || flight_val>=4000.0{
+              fmt.Println("error or light_val >4000:")
+              fmt.Println(err)
+              err = db.QueryRow("select light_val from light order by light_id desc limit 1;").Scan(&flight_val)
+              checkErr(err) 
+             // return
+        }
+        //иногда датчик выдает занчение больше 35000.0
+        /*if flight_val>=4000.0 {
+           err = db.QueryRow("select light_val from light where light order by light_id desc limit 1;").Scan(&flight_val)
+           checkErr(err)
+        }*/
+        insertLightval := fmt.Sprintf("%f",flight_val)
         fmt.Println("# Inserting values")
+        fmt.Println(flight_val)
         dt := time.Now()
         var lastInsertId int
-        err = db.QueryRow("INSERT INTO light (light_val,light_date) VALUES($1,$2) returning light_id;", light_val, dt).Scan(&lastInsertId)
+        err = db.QueryRow("INSERT INTO light (light_val,light_date) VALUES($1,$2) returning light_id;", insertLightval, dt).Scan(&lastInsertId)
         checkErr(err)
-        fmt.Println("last inserted id =", lastInsertId)
+        //fmt.Println("last inserted id =", lastInsertId)
 
 }
 
